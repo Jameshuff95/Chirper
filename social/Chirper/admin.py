b18525eeaf61a5dbb6580ebb5a8bfd6a50693ce3
James <jameshuff26@gmail.com>
2024-01-22 14:13:41 -0500

    created unique user profiles, added follow and unfollow functionality in admin backend site

--- social/Chirper/admin.py
+++ social/Chirper/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile 
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+# Mix profile info into User info in admin
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# Unregister initial user
+admin.site.unregister(User)
+
+# Re-register User
+admin.site.register(User, UserAdmin)
+
--- social/Chirper/models.py
+++ social/Chirper/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create a user profile model
+
+class Profile(models.Model):
+    # This associates one user with one profile, in other words, one user can only create one profile 
+    # ondelete means if the user is deleted so is the profile
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Profiles can follow many other profiles
+    # related_name is used for sql queries
+    # symmetrical means a user can follow a person without requiring the other person follow them back
+    # blank means users don't have to follow anyone
+    follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+
+    # Assign username to user profile in admin
+    def __str__(self):
+        return self.user.username
+
+    
