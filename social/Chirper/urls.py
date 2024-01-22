6a06695ea171aab4b08116b25c4d2dcaf69f8e3c
James <jameshuff26@gmail.com>
2024-01-22 18:39:44 -0500

    added followers and following sections to profile page, made individual profile pages for each user

--- social/Chirper/urls.py
+++ social/Chirper/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile_list/', views.profile_list, name='profile_list'),
+    # <int:pk> represents the primary key (id) of the user.
+    # This line makes the desired profile appear when called
+    path('profile/<int:pk>', views.profile, name='profile'),
 ]
