a72d3b3ef58552219e5fa98868306ccdb8004c51
James <jameshuff26@gmail.com>
2024-01-24 21:52:29 -0500

    added profile images to chirps and profile page, added styling to profile list page.

--- social/Chirper/forms.py
+++ social/Chirper/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Chirp
+from .models import Chirp, Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -57,3 +57,11 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted small">Enter the same password as before, for verfication.</span>'
+
+# Profile Extras Form
+class ProfilePicForm(forms.ModelForm):
+    profile_image = forms.ImageField(label="Profile Picture")
+
+    class Meta:
+        model = Profile
+        fields = ('profile_image', )
